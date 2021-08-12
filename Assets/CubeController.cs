ae5b77f0b0df1e755ee5e627b48ac8295a43d801
ledaig <ara@fiore-welfare.jp>
2021-08-12 14:38:46 +0900

    sound
    課題

--- Assets/CubeController.cs
+++ Assets/CubeController.cs
@@ -11,9 +11,14 @@ public class CubeController : MonoBehaviour
     // 消滅位置
     private float deadLine = -10;
 
+    //sound
+    AudioSource audioSource;
+    public AudioClip sound1;
+
     // Use this for initialization
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
 
     }
 
@@ -23,10 +28,22 @@ public class CubeController : MonoBehaviour
         // キューブを移動させる
         transform.Translate(this.speed * Time.deltaTime, 0, 0);
 
+
         // 画面外に出たら破棄する
         if (transform.position.x < this.deadLine)
         {
             Destroy(gameObject);
         }
     }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag != "UnityChan2D")
+        {
+            audioSource.PlayOneShot(sound1);
+            Debug.Log("collision");
+        }
+
+        //Debug.Log("Hit"); // 
+    }
+
 }
